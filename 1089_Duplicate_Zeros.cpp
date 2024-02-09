21674a4016f7c5c695ccdc7d02972ce15d624b13
techsamagan <156639202+techsamagan@users.noreply.github.com>
2024-02-09 13:11:15 -0600

    Create 1089_Duplicate_Zeros.cpp

---
+++ 1089_Duplicate_Zeros.cpp
@@ -0,0 +1,19 @@
+
+
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int n = arr.size();
+        int i = 0;
+
+        while (i < n) {
+            if (arr[i] == 0) {
+                arr.insert(arr.begin() + i, 0);
+                arr.pop_back(); 
+                i += 2; 
+            } else {
+                i++; 
+            }
+        }
+    }
+};
