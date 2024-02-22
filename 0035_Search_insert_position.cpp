62f62ad83b76b335ab0ea2dc7c544799b9bed157
techsamagan <156639202+techsamagan@users.noreply.github.com>
2024-02-22 12:11:24 -0600

    Create 0035_Search_insert_position.cpp

---
+++ 0035_Search_insert_position.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int size = nums.size();
+        for(int i=0; i<size; i++){
+            if(nums[i] == target){
+                return i;
+            }
+            else if(nums[i] > target){
+                return i;
+                break;
+            }
+        }
+        return size;
+    }
+};
